89de7bfb3971ac72deee0f8b614dfae180f18bc3
Thalys Row <thalysrogue@gmail.com>
2023-12-17 18:09:05 -0300

    index create

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import "dotenv/config";
+import mongoose from "mongoose";
+import router from "./routes/routes";
+
+mongoose
+  .connect(process.env.DB_URI!)
+  .then(() => {
+    const app = express();
+
+    app.use(express.json());
+    app.use(router);
+
+    app.listen(process.env.PORT, () => {
+      console.log("Running...");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
