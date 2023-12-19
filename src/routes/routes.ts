4a0b1b95f9b7ffa8d3f6077627987220690a0327
Thalys Row <thalysrogue@gmail.com>
2023-12-18 21:59:53 -0300

    add route new user

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
+import { UserController } from "../controllers/UsersControllers";
+import {
+  validateBodyUser,
+  validateUserCreate,
+} from "../middlewares/ValidateUser";
+import { newUser } from "../utils/schema";
 const router = Router();
 
-router.get("/", () => {
-  console.log("Hello, world!");
-});
+router.post(
+  "/user",
+  validateBodyUser(newUser),
+  validateUserCreate,
+  new UserController().create
+);
 
 export default router;
