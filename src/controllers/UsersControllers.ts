20b57bafe98cd5981ff4c56dcdb7d0f589939d7d
Thalys Row <thalysrogue@gmail.com>
2023-12-18 23:08:53 -0300

    add login user

--- src/controllers/UsersControllers.ts
+++ src/controllers/UsersControllers.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import User from "../models/User";
 import bcrypt from "bcrypt";
-import { formateData } from "../utils/UserFunctions";
+import { findUser, formateData } from "../utils/UserFunctions";
+import jwt from "jsonwebtoken";
 
 export class UserController {
   async create(req: Request, res: Response) {
@@ -21,6 +22,30 @@ export class UserController {
       return res.status(204).json();
     } catch (error) {
       return res.status(500).json({ message: "Erro in create user" });
+    }
+  }
+
+  async login(req: Request, res: Response) {
+    const { email } = req.body;
+    try {
+      const user = await findUser(email);
+      const userId = user!._id;
+      const token = jwt.sign({ id: userId }, process.env.JWT_PASS as string, {
+        expiresIn: 60 * 10,
+      });
+
+      const userInfo = {
+        id: user!._id,
+        photo: user!.photo,
+        name: user!.name,
+        user_name: user!.user_name,
+        email: user!.email,
+        createAt: user!.createdAt,
+      };
+
+      return res.status(201).json({ userInfo, token });
+    } catch (error) {
+      return res.status(500).json({ message: "Erro in login user" });
     }
   }
 }
