f61f349aac86124b1e745630195df933247861a3
Thalys Row <thalysrogue@gmail.com>
2023-12-19 18:59:58 -0300

    add schema update user

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -73,3 +73,68 @@ export const newUser = joi.object({
       custom: "Photo format invalid",
     }),
 });
+
+export const updateUser = joi.object({
+  name: joi
+    .string()
+    .max(50)
+    .custom((name, helpers) => {
+      if (name.trim() === "") {
+        return helpers.message({ message: "Format name invalid" });
+      }
+    })
+    .messages({
+      "string.empty": "The name field cannot be empty",
+      "string.max": "The name field must have a maximum of 50 characters",
+      custom: "Format name invalid",
+    }),
+
+  password: joi
+    .string()
+    .max(20)
+    .min(8)
+    .custom((password, helpers) => {
+      if (password.trim() === "" || password.includes(" ")) {
+        return helpers.message({
+          message: "Please check your password, no space allowed",
+        });
+      }
+    })
+    .messages({
+      "string.empty": "The password field cannot be empty",
+      "string.max": "The password field must have a maximum of 20 characters",
+      "string.min": "The password field must have a minimum of 8 characters",
+      custom: "Please check your password, no space allowed",
+    }),
+
+  email: joi.string().email().messages({
+    "string.empty": "The email field cannot be empty",
+    "string.email": "Email invalid format",
+  }),
+
+  user_name: joi
+    .string()
+    .max(10)
+    .custom((name, helpers) => {
+      if (name.trim() === "") {
+        return helpers.message({ message: "Format user_name invalid" });
+      }
+    })
+    .messages({
+      "string.empty": "The user_name field cannot be empty",
+      "string.max": "The user_name field must have a maximum of 10 characters",
+      custom: "Format name invalid",
+    }),
+
+  photo: joi
+    .string()
+    .custom((url, helpers) => {
+      if (url.trim() === "") {
+        return helpers.message({ message: "Url invalid" });
+      }
+    })
+    .messages({
+      "string.empty": "The photo field cannot be empty",
+      custom: "Format photo invalid",
+    }),
+});
