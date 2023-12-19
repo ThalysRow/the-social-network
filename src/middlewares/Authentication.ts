517f20298a221b3a4f57ef7565bc89d99993a69e
Thalys Row <thalysrogue@gmail.com>
2023-12-19 19:00:58 -0300

    fix req.userId

---
+++ src/middlewares/Authentication.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import User from "../models/User";
+interface CustomRequest extends Request {
+  userId?: number;
+}
+
+export const authentication = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { authorization } = req.headers;
+  try {
+    if (!authorization) {
+      return res.status(401).json({ message: "User not authentication" });
+    }
+
+    const token = authorization.split(" ")[1];
+    const user = jwt.verify(
+      token,
+      process.env.JWT_PASS as string
+    ) as JwtPayload;
+    req.userId = user.id;
+
+    const userFind = User.findById(req.userId);
+    if (!userFind) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    next();
+  } catch (error) {
+    return res.status(500).json({ message: "Erro in authentication" });
+  }
+};
