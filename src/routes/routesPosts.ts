11d51a635be5fba114d1256118a46f73ce22b4de
Thalys Row <thalysrogue@gmail.com>
2023-12-21 20:34:29 -0300

    add route post

---
+++ src/routes/routesPosts.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { validateNewPost } from "../middlewares/validatePost";
+import { NewPost } from "../utils/schema";
+import { PostController } from "../controllers/postsControllers";
+import multer from "../middlewares/multer";
+
+const routePosts = Router();
+
+routePosts.post(
+  "/post",
+  validateNewPost(NewPost),
+  multer.array("images"),
+  new PostController().newPost
+);
+
+export default routePosts;
