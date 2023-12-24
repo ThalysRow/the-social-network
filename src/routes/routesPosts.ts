34da705c2fc9042641128b94e9c8171e88618275
Thalys Row <thalysrogue@gmail.com>
2023-12-24 19:03:40 -0300

    rename route

--- src/routes/routesPosts.ts
+++ src/routes/routesPosts.ts
@@ -6,6 +6,6 @@ import multer from "../middlewares/multer";
 const routePosts = Router();
 
 routePosts.post("/post", multer.array("images"), new PostController().newPost);
-routePosts.get("/post", new PostController().get);
+routePosts.get("/posts", new PostController().get);
 
 export default routePosts;
