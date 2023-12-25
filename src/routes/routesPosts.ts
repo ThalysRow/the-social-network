710e0ce5a730da76ba3eca483bd51f55dfea5074
Thalys Row <thalysrogue@gmail.com>
2023-12-25 19:18:23 -0300

    to fixe route update post

--- src/routes/routesPosts.ts
+++ src/routes/routesPosts.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { PostController } from "../controllers/postsControllers";
 import multer from "../middlewares/multer";
+import { validateUpdatePost } from "../middlewares/validatePost";
 
 const routePosts = Router();
 
 routePosts.post("/post", multer.array("images"), new PostController().newPost);
 routePosts.get("/posts", new PostController().get);
 routePosts.get("/post/:id", new PostController().show);
-routePosts.patch("/post/:id", new PostController().update);
+routePosts.patch("/post/:id", validateUpdatePost, new PostController().update);
 
 export default routePosts;
